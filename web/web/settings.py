dff2258c755f00d432c79de1c9c258293a5ca952
Терещенко Дмитрий <dima123-99@mail.ru>
2019-09-26 22:22:30 +0300

    Added the connect to database.

--- web/web/settings.py
+++ web/web/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ChildCard',
+        'USER': 'postgres',
+        'PASSWORD': 'Flvby66!',
+        'HOST': 'aws-rds-postgresql.cfxoin7bynly.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
