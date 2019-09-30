e866074f0e28f8acd71f24f4a9ff8e049a181ba2
Терещенко Дмитрий <dima123-99@mail.ru>
2019-10-01 00:23:00 +0300

    Some changes.

--- ChildCard/wsgi.py
+++ ChildCard/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/opt/python/current/app')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ChildCard.settings'
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChildCard.settings')
-
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChildCard.settings')
 application = get_wsgi_application()
