eb84b124e598b188601a18652fbd5509122d0d80
Терещенко Дмитрий <dima123-99@mail.ru>
2019-10-02 01:19:23 +0300

    Forgotten to fix.

--- ChildCard/apps/accounts/forms.py
+++ ChildCard/apps/accounts/forms.py
@@ -9,7 +9,7 @@ class EmailValidationOnForgotPassword(PasswordResetForm):
     def clean_email(self):
         email = self.cleaned_data['email']
         if not User.objects.filter(email__iexact=email, is_active=True).exists():
-            msg = "Данный адрес электронной почты уже зарегистрирован."
+            msg = "Данный адрес электронной почты не зарегистрирован."
             self.add_error('email', msg)
         return email
 
