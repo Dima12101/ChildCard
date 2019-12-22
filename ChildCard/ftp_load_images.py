b51db1bdd39d0a7b5ec4d8c2da8fe51d60af31e5
Терещенко Дмитрий <dima123-99@mail.ru>
2019-12-22 20:31:05 +0300

    Logging ftp

--- ChildCard/ftp_load_images.py
+++ ChildCard/ftp_load_images.py
@@ -12,13 +12,23 @@ def download_file(name_file_server, path_file_client):
 		ftp.retrbinary('RETR ' + name_file_server, file_client.write)
 
 
+ftp_log = open('ftp_log.txt', 'w')
+
 print('Load images from FTP-server')
-print('>', ftp.connect('46.149.233.52', 30))
+ftp_log.write('Load images from FTP-server\n')
+
+response = ftp.connect('46.149.233.52', 30)
+print('>', response)
+ftp_log.write('> ' + response + '\n')
+
 print('> Login in FTP-server ...')
+ftp_log.write('> Login in FTP-server ...\n')
+
 ftp.login(os.environ['FTP_USER'], os.environ['FTP_PASSWORD'])
 
 ftp.cwd('ChildCard_images')
 print('> Loading...')
+ftp_log.write('> Loading...\n')
 for user_name in ftp.nlst():
 	if not os.path.exists(f'{BASE_DIR_CLIENT}/{user_name}'):
 		os.makedirs(f'{BASE_DIR_CLIENT}/{user_name}')
@@ -27,6 +37,9 @@ for user_name in ftp.nlst():
 		download_file(image_name, f'{BASE_DIR_CLIENT}/{user_name}/{image_name}')
 	ftp.cwd('..')
 print('> Success!')
+ftp_log.write('> Success!\n')
+
+ftp_log.close()
 
 ftp.close()
 
